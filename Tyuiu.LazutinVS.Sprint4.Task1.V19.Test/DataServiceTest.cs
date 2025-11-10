26e499847d2de88c383af1f16e23bce49f6a54f7
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-11-10 12:47:38 +0500

    Task1

---
+++ Tyuiu.LazutinVS.Sprint4.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.LazutinVS.Sprint4.Task1.V19.Lib;
+
+namespace Tyuiu.LazutinVS.Sprint4.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 6, 6, 9, 6, 9, 3, 8, 2, 9, 5, 7, 4 };
+            int res = ds.Calculate(numsArray);
+            int wait = 31;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint4.Task1.V19/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.LazutinVS.Sprint4.Task1.V19.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int len;
+        Console.WriteLine("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+
+
+        int[] numsArray = new int[len];
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("Введите значение " + i + " Элемент массива: ");
+            numsArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int res = ds.Calculate(numsArray);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
