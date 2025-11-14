ea61e58641381a978f7431e8b21ed2d313bc0dc3
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-11-14 22:31:33 +0500

    Task7861786

--- Tyuiu.LazutinVS.Sprint4.Task7.V26.Test/DataServiceTest.cs
+++ Tyuiu.LazutinVS.Sprint4.Task7.V26.Test/DataServiceTest.cs
@@ -7,7 +7,13 @@ namespace Tyuiu.LazutinVS.Sprint4.Task7.V26.Test
         [TestMethod]
         public void TestMethod1()
         {
-            
+            DataService ds = new DataService();
+            string str = "351268459614723";
+            int r = 3;
+            int c = 5;
+            int wait = 18432;
+            int res = ds.Calculate(r, c, str);
+            Assert.AreEqual(wait, res);
         }
     }
 }
