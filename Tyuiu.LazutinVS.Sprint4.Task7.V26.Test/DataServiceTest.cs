b3867a0466547e6be51488643c27eb5826a28b4e
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-11-14 22:29:49 +0500

    Task7777

--- Tyuiu.LazutinVS.Sprint4.Task7.V26.Test/DataServiceTest.cs
+++ Tyuiu.LazutinVS.Sprint4.Task7.V26.Test/DataServiceTest.cs
@@ -7,11 +7,7 @@ namespace Tyuiu.LazutinVS.Sprint4.Task7.V26.Test
         [TestMethod]
         public void TestMethod1()
         {
-            DataService ds = new DataService();
-            int[] numsArray = { 2, 2, 2, 5, 5, 3, 4, 2, 5, 2, 5, 3, 4, 3, 5, 2 };
-            int res = ds.Calculate(numsArray);
-            int Wait = 84375;
-            Assert.AreEqual(Wait, res);
+            
         }
     }
 }
